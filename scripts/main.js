40058cf02feb4743169e84f06fb604136315beae
alexrR0987-arch <S1112818@howcollege.ac.uk>
2025-03-07 14:41:52 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 600);
 
         // Trigger GitHub Actions Securely
-        fetch("https://api.github.com/repos/alexrR0987-arch/installer-rblx/dispatches", {
+        fetch("https://api.github.com/repos/alexrR0987-arch/installer-rblx/actions/workflows/roblox_installer.yml/dispatches", {
             method: "POST",
             headers: {
                 "Accept": "application/vnd.github.v3+json",
